869d60dbf2202fe66ae447cb87eab7ff251bfec0
Mihail Kasanov <mixal1302002@gmail.com>
2022-10-11 23:27:28 +0500

    added some git commands

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /*
+        git config --global user.name ""
+        git config --global user.email
+        git config --list
+
+        git init
+        git add .
+        git commit -m "commit description"
+        git remote add origin "repository path"
+        git push -u origin <master>
+        * */
     }
+
 }
